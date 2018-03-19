06ca867fc18669abdaeadb74c4c9c87b253a7ae9
Luiz Mascarenhas <luiz.mascarenhas@mobicare.com.br>
2018-03-19 11:13:43 -0300

    Ajustes criação Jenkins Server

--- aws-resources/roles/ec2_create_jenkins/files/user_data.sh
+++ aws-resources/roles/ec2_create_jenkins/files/user_data.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e -x
 
-yum update -y
-pip install ansible
-cd /root/ && git clone https://github.com/jenkinsci/docker.git
-cd /root/docker
-sed -i 's/\/var\/jenkins_home/\/var\/jenkins_home:\/var\/jenkins_home/' docker-compose.yml
-mkdir -p /var/jenkins_home
-chmod 777 /var/jenkins_home
-docker-compose up -d --build
+usermod -aG docker bitnami
+usermod -aG docker tomcat
+usermod -aG docker ubuntu
